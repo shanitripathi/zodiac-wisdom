26a2a6248f9fdb5dbf5406584483d0fa25e2388d
Shani Tripathi <shani.tripathi@axonista.com>
2024-04-12 17:39:16 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1 class="text-2xl text-neutral-100 bg-neutral-800">
+	This is the boilerplate for sveltekit projects created by Shani.
+</h1>
---
+++ svelte.config.js
@@ -0,0 +1,26 @@
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
+		alias: {
+			$components: path.resolve('./src/lib/components'),
+			$stores: path.resolve('./src/lib/stores'),
+			$screens: path.resolve('./src/lib/screens'),
+			$assets: path.resolve('./src/lib/assets'),
+			$helpers: path.resolve('./src/lib/helpers')
+		}
+	}
+};
+
+export default config;
